ae2ee4148263d666e60f2181c3a6482268665034
lfelipeeuzebio <lfelipeeuzebio@gmail.com>
2025-12-28 23:41:45 -0300

    Update app_escala.py

--- app_escala.py
+++ app_escala.py
@@ -42,6 +42,36 @@ if busca:
 else: 
     st.warning("Nenhum dia encontrado, tente novamente!")
 
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"] {
+    background-image: url("https://github.com/lfelipeeuzebio/Vers-o-inicial-do-app/blob/a85fe1f94328fb2a21836e0213ddaa7bd486f5f5/fun_paroquia.jpeg");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+}
+
+[data-testid="stAppViewContainer"]::before {
+    content: "";
+    position: absolute;
+    top: 0; left: 0; right: 0; bottom: 0;
+    background: rgba(0, 0, 0, 0.5);  
+    pointer-events: none;
+}
+
+[data-testid="stAppViewContainer"] > * {
+    position: relative;
+    z-index: 1;
+}
+
+[data-testid="stHeader"] {
+    background-color: rgba(0,0,0,0);
+}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
 
 
